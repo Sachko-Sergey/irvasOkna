424d009747a520e9b4e8690379493ea92bbb6463
Sachko-Sergey <sachkosergey11@gmail.com>
2020-01-25 16:50:42 +0300

    add block tabs

--- js/common.js
+++ js/common.js
@@ -9,4 +9,12 @@ $(document).ready(function () {
   clock.setTime(220880);
   clock.setCountdown(true);
   clock.start();
+
+  $(".tab_item-inner").not(":first").hide();
+  $(".wrapper .tab").click(function () {
+    $(".wrapper .tab").removeClass("active").eq($(this).index()).addClass("active");
+    $(".tab_item-inner").hide().eq($(this).index()).fadeIn()
+  }).eq(0).addClass("active");
+
+  
 });
